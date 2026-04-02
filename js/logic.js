f620c08e28d8e09659f6473fc5e0faed509821f6
yumonwilliams <caddilacwarlock@gmail.com>
2026-04-02 11:45:06 -0400

    Added lots of stuff

---
+++ js/logic.js
@@ -0,0 +1,36 @@
+//Dropdown menu elements IDs for game levels. 
+const level001 = document.getElementById('level-001');
+level001.addEventListener('click', function() {
+    console.log('001');
+});
+
+const level002 = document.getElementById("level-002");
+level002.addEventListener('click', function() {
+    console.log('002');
+});
+
+const level003 = document.getElementById("level-003");
+level003.addEventListener('click', function() {
+    console.log('003');
+});
+
+const level004 = document.getElementById("level-004");
+level004.addEventListener('click', function() {
+    console.log('004');
+});
+
+const level005 = document.getElementById("level-005");
+level005.addEventListener('click', function() {
+    console.log('005');
+});
+
+const level006 = document.getElementById("level-006");
+level006.addEventListener('click', function() {
+    console.log('006');
+});
+
+const level007 = document.getElementById("level-007");
+level007.addEventListener('click', function() {
+    console.log('007');
+})
+
